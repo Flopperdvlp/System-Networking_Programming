88d6d2ea53fb83b6e124b303584de2ab1aaeebf3
Flopperdvlp <lp021378@gmail.com>
2024-12-04 00:32:52 +0200

    Add folder

---
+++ Web-Virus/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace WpfApp6
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWebsites_Click(object sender, RoutedEventArgs e)
+        {
+            string[] urls =
+            {
+                "https://www.google.com",
+                "https://www.youtube.com",
+                "https://www.facebook.com",
+                "https://www.twitter.com"
+            };
+
+            foreach (var url in urls)
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true 
+                });
+            }
+        }
+    }
+}
