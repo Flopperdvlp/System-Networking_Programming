f0baaa880a95354ae54d0fb46cc51004650be611
Flopperdvlp <lp021378@gmail.com>
2024-12-08 12:56:32 +0200

    Add folder

---
+++ Internet-Manager/Internet-Manager/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using SharpPcap;
+using System.Linq;
+using System.Windows;
+
+namespace Internet_Manager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadInformation();
+        }
+        private void LoadInformation()
+        {
+            CaptureDeviceList deviceList = CaptureDeviceList.Instance;
+            var list = deviceList.Select(device => new { Name = device.Name, Description = device.Description, Status = CheckDeviceStatus(device) }).ToList();
+            var devices = list;
+            DevicesTable.ItemsSource = devices;
+        }
+        private string CheckDeviceStatus(ICaptureDevice device)
+        {
+            try
+            {
+                device.Open(); 
+                device.Close();
+                return "Available";
+            }
+            catch
+            {
+                return "Unavailable";
+            }
+        }
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadInformation();
+        }
+    }
+}
