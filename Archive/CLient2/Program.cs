ded28da0e08f9b187cf268c426854b77b998156b
Lyagushka <lp021378@gmail.com>
2024-12-25 17:53:04 +0200

    Update 2

---
+++ Archive/CLient2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+public class Client1
+{
+    public static void Main()
+    {
+        using (var client = new TcpClient("127.0.0.1", 5000))
+        using (var stream = client.GetStream())
+        {
+            Console.WriteLine("Введіть ваше повідомлення:");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message)) continue;
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Повідомлення відправлено.");
+            }
+        }
+    }
+}
---
+++ Archive/Client1 copy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+public class Client2
+{
+    public static void Main()
+    {
+        using (var client = new TcpClient("127.0.0.1", 5000))
+        using (var stream = client.GetStream())
+        {
+            System.Threading.Thread receiveThread = new System.Threading.Thread(() => ReceiveMessages(stream));
+            receiveThread.Start();
+            receiveThread.Join();
+        }
+    }
+
+    public static void ReceiveMessages(NetworkStream stream)
+    {
+        byte[] buffer = new byte[1024];
+        while (true)
+        {
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead == 0) break;
+
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Отримано повідомлення: " + message);
+        }
+    }
+}
